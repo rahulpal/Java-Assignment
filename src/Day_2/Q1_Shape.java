1e12f5b972e2bd3ae78b0016527b8fb244d42389
rahulpal <rahulpal1110@gmail.com>
2024-09-24 19:11:20 +0530

    new commit

---
+++ src/Day_2/Q1_Shape.java
@@ -0,0 +1,38 @@
+package Day_2;
+
+ class Shape {
+	
+	private int width;
+	private int height;
+	
+	public void setWidth(int width) {
+		
+		this.width = width;		
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+
+}
+
+ public class Q1_Shape{
+	 public static void main(String [] args) {
+		 
+		 Shape shape = new Shape();
+		 shape.setWidth(10);
+		 shape.setHeight(10);
+		 
+		 System.out.println(shape.getWidth());
+		 System.out.println(shape.getHeight());
+	 }
+ }
+
